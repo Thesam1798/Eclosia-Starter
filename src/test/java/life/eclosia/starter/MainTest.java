8651d97017b7d7a0291be465132deca32d978bee
Alexandre <debris.alex@gmail.com>
2020-06-09 22:51:51 +0200

    Init project

---
+++ src/test/java/life/eclosia/starter/MainTest.java
@@ -0,0 +1,19 @@
+package life.eclosia.starter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
